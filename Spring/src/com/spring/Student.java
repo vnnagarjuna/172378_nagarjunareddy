4fd60a72abbfea9e82993cdfc7239bff035b538c
vnnagarjuna <47312134+vnnagarjuna@users.noreply.github.com>
2019-03-14 17:36:51 +0530

    Add files via upload

---
+++ Spring/src/com/spring/Student.java
@@ -0,0 +1,18 @@
+package com.spring;
+
+public class Student {
+	private String name;  
+	  
+	public String getName() {  
+	    return name;  
+	}  
+	  
+	public void setName(String name) {  
+	    this.name = name;  
+	}  
+	  
+	public void displayInfo(){  
+	    System.out.println("Hello: "+name);  
+	}  
+
+}
