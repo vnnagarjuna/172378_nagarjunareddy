aa12001132dfd8ef71efa8deb2fc874c38f03c3c
vnnagarjuna <47312134+vnnagarjuna@users.noreply.github.com>
2019-03-14 17:25:20 +0530

    Add files via upload

---
+++ Demo1/WebContent/Clock.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1" import="java.util.Date"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Clock</title>
+</head>
+<body>
+the time is:<%=new Date()%>
+</body>
+</html>
---
+++ Demo1/src/com/capg/Demo1Servlet.java
@@ -0,0 +1,47 @@
+package com.capg;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Demo1Servlet
+ */
+@WebServlet("/Demo1ServletPath")
+public class Demo1Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Demo1Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("in do get");
+		PrintWriter out=response.getWriter();
+		String user=request.getParameter("username");
+		out.print("hello "+user);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
